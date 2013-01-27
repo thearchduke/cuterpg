660c4cac7fb5c24957ab14c3a5b5f64c40423a64
thearchduke <tynanburke@gmail.com>
2013-01-26 21:54:21 -0800

    10pm commit

---
+++ cuterpg/db.py
@@ -0,0 +1,10 @@
+import sqlalchemy
+
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+engine = create_engine('sqlite:///cuterpg.db', echo=True)
+Base = declarative_base()
+
